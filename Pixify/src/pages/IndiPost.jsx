e26281cfa479e8744d98dfb156d853a92d623513
Aditya <adikpote@gmail.com>
2022-10-29 09:58:10 +0530

    Modified pages/Indipost functionality

--- Pixify/src/pages/IndiPost.jsx
+++ Pixify/src/pages/IndiPost.jsx
@@ -1,33 +1,68 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getPostedPosts } from "../features/pixify/pixifySlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  getIndiPost,
+  onUnlike,
+  onLike,
+  resetCurrPost,
+  getUserNameFromUserId,
+  deletePost,
+} from "../features/pixify/pixifySlice";
 import ClipLoader from "react-spinners/ClipLoader";
-import Card from "../components/Card";
-import LoadMore from "../components/LoadMore";
+import { FaHeart, FaTrash } from "react-icons/fa";
 
-const MyPosts = () => {
+const IndiPost = () => {
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { userId, postedPosts } = useSelector((store) => store.pixify);
-
-  const fetchPostedPosts = async () => {
+  const { currentPost, userId } = useSelector((store) => store.pixify);
+  const { postUserName } = currentPost;
+  const { id } = useParams();
+  const fetchIndiPost = async () => {
     setLoading(true);
-    await dispatch(getPostedPosts({ userId }));
+    let response = await dispatch(getIndiPost(id));
+    if (response.error) {
+      navigate("/");
+    }
+    response = await dispatch(
+      getUserNameFromUserId({ userId: response.payload.postUserId })
+    );
+    if (response.error) {
+      navigate("/");
+    }
     setLoading(false);
   };
 
+  const handleOnClickLike = async () => {
+    await dispatch(onLike({ id, userId }));
+    fetchIndiPost({ userId });
+  };
+
+  const handleOnClickUnlike = async () => {
+    await dispatch(onUnlike({ id, userId }));
+    fetchIndiPost({ userId });
+  };
+
+  const handleOnClickDelete = async () => {
+    const response = await dispatch(deletePost({ id }));
+    setLoading(false);
+    navigate("/myposts");
+  };
+
   useEffect(() => {
-    const fetch = async () => {
-      await fetchPostedPosts();
+    setLoading(true);
+    fetchIndiPost();
+    return () => {
+      dispatch(resetCurrPost());
     };
-    fetch();
-  }, []);
+  }, [dispatch, id]);
 
   if (loading) {
     return (
       <section className="w-full">
         <div className="p-8">
-          <div className="max-w-[500px] m-auto">
+          <div className="max-w-[400px] m-auto">
             <div className="w-full flex items-center justify-center">
               <ClipLoader loading={loading} size={50} color="white" />
             </div>
@@ -35,42 +70,49 @@ const MyPosts = () => {
         </div>
       </section>
     );
-  }
-
-  if (postedPosts.length === 0) {
+  } else {
     return (
       <section className="w-full">
         <div className="p-8">
-          <div className="m-auto max-w-[1000px]">
-            <div className="flex items-center justify-center">
-              <div className="text-xl font-bold">You haven't posted Yet</div>
+          <div className="max-w-[400px] m-auto">
+            <div className="flex flex-col w-full">
+              <div className="m-auto w-full">
+                <img
+                  src={currentPost.imageUrl}
+                  alt={currentPost.title}
+                  className="w-full rounded-2xl shadow-2xl"
+                />
+              </div>
+              <div className="m-auto w-full mt-8 flex flex-col justify-between">
+                <div className="flex items-center justify-between w-full">
+                  <div className="font-bold text-2xl">{currentPost.title}</div>
+                  <div className="flex items-center justify-center">
+                    <div className="hover:scale-105 duration-300 cursor-pointer">
+                      {currentPost.likedByLoggedInUser ? (
+                        <FaHeart
+                          color="red"
+                          onClick={(id) => handleOnClickUnlike(id)}
+                        />
+                      ) : (
+                        <FaHeart onClick={(id) => handleOnClickLike(id)} />
+                      )}
+                    </div>
+                    <div className="ml-2 w-2">{currentPost.likedBy.length}</div>
+                    {currentPost.postUserId == userId ? (
+                      <div className="ml-4 hover:scale-105 duration-300 cursor-pointer">
+                        <FaTrash onClick={handleOnClickDelete} />
+                      </div>
+                    ) : null}
+                  </div>
+                </div>
+                {postUserName}
+              </div>
             </div>
           </div>
         </div>
       </section>
     );
   }
-
-  return (
-    <>
-      <section className="w-full">
-        <div className="p-8 pt-2">
-          <div className="m-auto max-w-[1000px] ">
-            <div className="mb-8">
-              <p className="font-bold text-xl">My posts</p>
-            </div>
-            <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4">
-              {postedPosts.map((post) => {
-                return <Card {...post} key={post.id} />;
-              })}
-            </div>
-          </div>
-        </div>
-      </section>
-      <LoadMore issuer="MyPosts" />
-    </>
-  );
 };
 
-export default MyPosts;
-
+export default IndiPost;
